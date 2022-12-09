e6ce452037970fdae6672994ae31314a58c05887
Vilem Lipold <vilem.lipold@studentagency.cz>
2022-12-09 20:37:22 +0100

    Removed bad attribs

--- GeocachingMapEnhancements/GeocachingMapEnhancements.meta.js
+++ GeocachingMapEnhancements/GeocachingMapEnhancements.meta.js
@@ -2,15 +2,11 @@
 // @name        Geocaching Map Enhancements
 // @version     0.9.3
 // @author      JRI, Vylda
-// @oujs:author JRI, Vylda
 // @namespace   inge.org.uk/userscripts
 // @description Adds extra maps and grid reference search to Geocaching.com, along with several other enhancements.
-// @include     https://www.geocaching.com/*
+// @match       https://www.geocaching.com/*
 // @license     MIT; http://www.opensource.org/licenses/mit-license.php
 // @copyright   2011-18, James Inge (http://geo.inge.org.uk/)
-// @attribution GeoNames (http://www.geonames.org/)
-// @attribution Postcodes.io (https://postcodes.io/)
-// @attribution Chris Veness (http://www.movable-type.co.uk/scripts/latlong-gridref.html)
 // @grant       GM_xmlhttpRequest
 // @grant       GM.xmlHttpRequest
 // @connect     geograph.org.uk
